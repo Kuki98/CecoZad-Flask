32772905a3e7fc1492d9169060b60f0bf1c1c41c
Pr0phet <pr0phet@abv.bg>
2019-02-11 22:20:18 +0200

    Job done!

--- application.py
+++ application.py
@@ -21,7 +21,7 @@ def index():
 
 @app.route('/<pic_name>')
 def view_comment(pic_name):
-	query= 'SELECT comment, comment_id, img_id\
+	query= 'SELECT comment, comment_id, img_name\
 			FROM commentsTable'
 	cursor.execute(query)
 	return render_template('static_page.html', names=cursor, pic_name=pic_name)
@@ -30,8 +30,8 @@ def view_comment(pic_name):
 def submit_comment(pic_name=None):
 	#get comment and insert into base
 	if request.method == 'POST':
-		query = "INSERT INTO commentsTable(comment, img_id)\
-				VALUES('%s', '%s')" % (request.form['comment'], request.form['image-id'])
+		query = "INSERT INTO commentsTable(comment, img_name)\
+				VALUES('%s', '%s')" % (request.form['comment'], request.form['image-name'])
 		cursor.execute(query)
 		db.commit()
 		return redirect(url_for('view_comment', pic_name=pic_name))
@@ -48,7 +48,7 @@ def delete_comment(pic_name=None):
 @app.route('/edit_comment/<pic_name>', methods=['POST', 'GET'])
 def edit_comment(pic_name=None):
 	edit_value = request.form['edit_comment']	
-	return render_template('edit_page.html', edit_value=edit_value)
+	return render_template('edit_page.html', edit_value=edit_value, pic_name=pic_name)
 
 @app.route('/update_comment/<pic_name>', methods=['POST', 'GET'])
 def update_comment(pic_name=None):
@@ -59,3 +59,13 @@ def update_comment(pic_name=None):
 		cursor.execute(query)
 		db.commit()
 	return redirect(url_for('view_comment', pic_name=pic_name))
+@app.route('/upload_file', methods=['POST', 'GET'])
+def upload_file():
+	if request.method == 'POST':
+		f = request.files['image_upload']
+		f.save('/home/pr0phet/MyProjects/Web/static/' + f.filename)
+		query = "INSERT INTO imageTable(img_path, img_name)\
+				VALUES('%s', '%s')" % (('/static/' + f.filename), (f.filename))
+		cursor.execute(query)
+		db.commit()
+		return redirect(url_for('index'))
