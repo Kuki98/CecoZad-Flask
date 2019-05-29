67cb0715329b3026dbacbb581f237b9faf6f1d4d
pr0phet <thesparta1999@gmail.com>
2019-05-29 12:44:25 +0300

    Fixed 500 response. Working Ajax request written on vanilla JavaScript.

--- static/VanillaJsIndex.js
+++ static/VanillaJsIndex.js
@@ -4,12 +4,14 @@ document.getElementById('CommentEnter').addEventListener('submit', function (eve
     var cnx = new XMLHttpRequest();
     var forms = document.getElementById('CommentEnter').getElementsByTagName('form')[0];
     var url = forms.getAttribute('action')
-    var data = document.getElementById('comment').value;
+    var commentData = document.getElementById('comment').value;
+    var formData = new FormData(forms);
 
     cnx.open('POST', url);
+
     cnx.onload = function(){
         document.getElementById('CommentShow').innerHTML += '<p>' + data + '</p>';
     }
 
-    cnx.send();
+    cnx.send(formData);
 });
