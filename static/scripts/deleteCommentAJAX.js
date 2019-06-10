85cb8cd72709c5a8ed08bf2ff458225aa95a51f6
pr0phet <thesparta1999@gmail.com>
2019-06-10 18:34:40 +0300

    Refactored ajax

--- static/scripts/deleteCommentAJAX.js
+++ static/scripts/deleteCommentAJAX.js
@@ -1,26 +1,23 @@
 $(function () {
     $('#CommentShow form').on('submit', function (event) {
         event.preventDefault();
+        console.log($(this))
 
-        var url = $(this).attr('action');
+        var url = $('#delete-button').attr('formaction')
 
         var deleteCommentId = $('#delete-button', this).val();
 
-//        var comment = $('#comment_id').text().trim()
-
         var data = {
-            delete_comment_name:deleteCommentId
+            delete_comment_name: deleteCommentId
         };
         var ajaxData = {
-            url: url,
-            data: data,
-            method: 'POST',
-            success: function(){
-                alert('Successfully deleted comment: ')
-
-            },
-        };
-        console.log(ajaxData)
+            url : url,
+            data : data,
+            method : "POST",
+            success : function() {
+                $(this).empty()
+            }
+        }
         $.ajax(ajaxData)
     });
 });
